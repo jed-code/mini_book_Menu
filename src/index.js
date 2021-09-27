57b172802d4f44b9c00ef982ddc95350433da51f
Jed Kazadi <56468464+jed-code@users.noreply.github.com>
2021-09-27 11:25:24 +0200

    Book Menu

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import "./index.css";
+
+function Booklist() {
+  return (
+    <div className="booklist">
+      <Books />
+      <Books />
+      <Books />
+      <Books />
+    </div>
+  );
+}
+
+const Books = () => {
+  return (
+    <div className="book">
+      <Image />
+      <Title />
+      <Author />
+    </div>
+  );
+};
+
+const Title = () => <h4>The Rise And Fall</h4>;
+const Author = () => <p>VanderBilt</p>;
+const Image = () => (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/41H5veGs31S._AC_SX184_.jpg" />
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Booklist />
   </React.StrictMode>,
   document.getElementById("root")
 );
